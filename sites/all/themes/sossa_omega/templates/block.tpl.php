6de2a12ec1946f907a1184ce3411d32c904bccfd
Marlene Williams <marlene@designbymarlene.com>
2011-12-04 22:34:33 -0800

    Some fleshing out of the initial theme

---
+++ sites/all/themes/sossa_omega/templates/block.tpl.php
@@ -0,0 +1,16 @@
+<?php $tag = $block->subject ? 'section' : 'div'; ?>
+<<?php print $tag; ?><?php print $attributes; ?>>
+  <div class="block-inner clearfix">
+    <div class="block-title-wrapper">
+    <?php print render($title_prefix); ?>
+    <?php if ($block->subject): ?>
+      <h2<?php print $title_attributes; ?>><?php print $block->subject; ?></h2>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+    </div><!-- /.block-title-wrapper -->
+    
+    <div<?php print $content_attributes; ?>>
+      <?php print $content ?>
+    </div>
+  </div>
+</<?php print $tag; ?>>
